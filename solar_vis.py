0a999de54d10d76e54d7736e219a18eec8a94431
Полина <>
2016-11-10 15:57:35 +0300

    Changed solar_vis.py

--- solar_vis.py
+++ solar_vis.py
@@ -3,7 +3,8 @@
 
 """Модуль визуализации.
 Нигде, кроме этого модуля, не используются экранные координаты объектов.
-Функции, создающие гaрафические объекты и перемещающие их на экране, принимают физические координаты
+Функции, создающие гaрафические объекты и перемещающие их на экране, принимают
+физические координаты
 """
 
 header_font = "Arial-16"
@@ -22,7 +23,8 @@ scale_factor = None
 
 
 def calculate_scale_factor(max_distance):
-    """Вычисляет значение глобальной переменной **scale_factor** по данной характерной длине"""
+    """Вычисляет значение глобальной переменной **scale_factor**
+    по данной характерной длине"""
     global scale_factor
     scale_factor = 0.4*min(window_height, window_width)/max_distance
     print('Scale factor:', scale_factor)
@@ -54,7 +56,7 @@ def scale_y(y):
     **y** — y-координата модели.
     """
 
-    return y  # FIXME: not done yet
+    return int(-y*scale_factor) - window_height//2
 
 
 def create_star_image(space, star):
@@ -80,7 +82,10 @@ def create_planet_image(space, planet):
     **space** — холст для рисования.
     **planet** — объект планеты.
     """
-    pass  # FIXME: сделать как у звезды
+    x = scale_x(planet.x)
+    y = scale_y(planet.y)
+    r = planet.R
+    planet.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=planet.color)
 
 
 def update_system_name(space, system_name):
